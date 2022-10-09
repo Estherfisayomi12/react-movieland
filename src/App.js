30d76bad6b9a5b285c0a521f8b758f4dc441a134
Estherfisayomi12 <fisayomi2602@gmail.com>
2022-10-09 20:03:24 +0100

    changed http to https

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SearchIcon from "./search.svg";
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=214b4227";
+const API_URL = "https://www.omdbapi.com?apikey=214b4227";
 
 const movie1 = {
   Title: "Amazing",
